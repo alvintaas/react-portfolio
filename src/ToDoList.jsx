e6a8e81dcda9f489a30ac4167dd5f8b2a74c9142
ALVIN TAAS <alvintaas@gmail.com>
2025-05-27 11:05:22 +1000

    completed tutorial, and edited css

--- src/ToDoList.jsx
+++ src/ToDoList.jsx
@@ -11,32 +11,53 @@ function ToDoList({ todos, onDelete }) {
     }
 
     function addTask() {
-
+        
+        if(newTask.trim() !== '') {
+        
+        setTasks(t => [...t, newTask]);
+        setNewTask(''); // Clear input after adding
+        }
     }
 
     function deleteTask(index) {
-
+        const updatedTasks = tasks.filter((_, i) => i !== index);
+        setTasks(updatedTasks);
     }
 
     function moveTaskUp(index) {
+
+        if (index > 0) {
+            const updatedTasks = [...tasks];
+            const temp = updatedTasks[index - 1];
+            updatedTasks[index - 1] = updatedTasks[index];
+            updatedTasks[index] = temp;
+            setTasks(updatedTasks);
+        }
     }
 
     function moveTaskDown(index) {
-
+        if (index < tasks.length - 1) {
+            const updatedTasks = [...tasks];
+            const temp = updatedTasks[index + 1];
+            updatedTasks[index + 1] = updatedTasks[index];
+            updatedTasks[index] = temp;
+            setTasks(updatedTasks);
+        }
     }
 
     return(
     <div className="to-do-list">
-        <h1>To Do List</h1>
+        <div className='card-heading'>
+            <h2>To Do List</h2>
+        </div>
 
-        <div>
+        <div className="to-do-list-input-container">
             <input
             type='text'
             placeholder='Enter a task..'
             value ={newTask}
             onChange={handleInputChange}
             /> 
-            <br></br>
             <button className='add-button'
             onClick={addTask}>
             Add Task</button>
@@ -46,11 +67,12 @@ function ToDoList({ todos, onDelete }) {
         <ol>
             {tasks.map((task, index) => 
                 <li key={index}>
-                    <span className='text'>{task}</span>
-                    
-                    <button className='delete-button' onClick={() => deleteTask(index)}>Delete</button>
-                    <button className='move-up-button' onClick={() => moveTaskUp(index)}>Move Up</button>
-                    <button className='move-down-button' onClick={() => moveTaskDown(index)}>Move Down</button>
+                    <span className='todo-text'>{task}</span>
+                    <span className='todo-actions'>
+                        <button className='delete-button' onClick={() => deleteTask(index)}>🗑️</button>
+                        <button className='move-button' onClick={() => moveTaskUp(index)}>🔼</button>
+                        <button className='move-button' onClick={() => moveTaskDown(index)}>🔽</button> 
+                    </span>
                 </li>
             )}
         </ol>
